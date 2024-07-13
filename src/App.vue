5f87627eea117148260a3f89260f40bfe708504d
belajar-vue-2 <aryaadi229@gmail.com>
2024-07-13 19:35:07 +0700

    Menyimpan Nilai State Dengan Reactive

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <main :style="{ backgroundColor: `rgb(${red}, ${green}, ${blue})` }">
-    <input type="number" placeholder="red" v-model="red" />
-    <input type="number" placeholder="green" v-model="green" />
-    <input type="number" placeholder="blue" v-model="blue" />
+  <main
+    :style="{
+      backgroundColor: `rgb(${color.red}, ${color.green}, ${color.blue})`,
+    }"
+  >
+    <input type="number" placeholder="red" v-model="color.red" />
+    <input type="number" placeholder="green" v-model="color.green" />
+    <input type="number" placeholder="blue" v-model="color.blue" />
   </main>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const red = ref(0);
-const green = ref(0);
-const blue = ref(0);
+
+// const red = ref(0);
+// const green = ref(0);
+// const blue = ref(0);
+
+const color = ref({
+  red: 0,
+  green: 0,
+  blue: 0,
+});
 </script>
 
 <style scoped>
 main {
-  min-height: 100vh;
+  height: 100vh;
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 0%;
-  padding: 0%;
+  margin: 0;
+  padding: 0;
 }
 </style>
